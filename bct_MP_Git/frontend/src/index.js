14b6de0914e2eb741a572a188f26bc494bba6536
vat03 <100333733+vat03@users.noreply.github.com>
2024-10-30 20:13:45 +0530

    Delete bct_MP_Git directory

--- bct_MP_Git/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-// frontend/src/index.js
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
